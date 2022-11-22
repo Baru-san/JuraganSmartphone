325bbf9bef9a4165dec5a69470d18a660f9fdbdd
Muhammad Akbarul Ihsan <barullisan@gmail.com>
2022-11-22 16:59:00 +0700

    Interaksi Login

--- JurganSmartphone/src/main/java/main/Main.java
+++ JurganSmartphone/src/main/java/main/Main.java
@@ -1,24 +1,13 @@
 package main;
 
-import Classes.Smartphone;
-import Databases.*;
+import GUI.*;
 
 public class Main {
     
     public static void main(String args[]) {
         
-        Database db = new Database();
-        db.buatObjekSmartphone();
-        
-        //Smartphone[] sp = new Smartphone[20];
-        
-//        sp = db.buatObjekSmartphone();
-//        
-//            for(int i=0;i<3;i++){
-//            System.out.println(sp[i].getNama());
-//            System.out.println(sp[i].getHarga());
-//            System.out.println(sp[i].getRating());
-//            //System.out.println(namamenu[i].getJenis());
-//        }
+        java.awt.EventQueue.invokeLater(() -> {
+          new Login().setVisible(true);  
+        });
     }
 }
